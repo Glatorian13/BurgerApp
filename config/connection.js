f21ab5d8dd35dc4a9dffd7c47cc458d45c5fb832
Javier Cardenas <glatorian@gmail.com>
2021-03-11 21:37:24 -0500

    final hbs

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
     port: 3306,
-    user: 'root',
-    password: '',
-    database: 'burgers_db',
-});
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
 
-connection.connect(function(err) {
+  connection.connect(function (err) {
     if (err) {
-        console.error(`error connecting: ${err.stack}`);
-        return;
+      console.error(`error connecting: ${err.stack}`);
+      return;
     }
     console.log(`connected as threadId: ${connection.threadId}`);
-});
+  });
+}
 
 module.exports = connection;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,40 +1,44 @@
-const express = require('express');
-const burger = require('../models/burger.js');
+const express = require("express");
+const burger = require("../models/burger.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    burger.selectAll((data) => {
-        const hbsObject = {
-            burgers: data,
-        };
-        console.log(hbsObject);
-        res.render('index', hbsObject);
-    });
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-router.post('/api/burgers', (req, res) => {
-    burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
-        res.json({ id: result.insertId });
-    });
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    (result) => {
+      res.json({ id: result.insertId });
+    }
+  );
 });
 
-router.put('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
-    console.log('condition', condition);
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;
+  console.log("condition", condition);
 
-    burger.updateOne(
-        {
-            devoured: req.body.devoured,
-        },
-        condition,
-        (result) => {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
-        }
-    );
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
 });
 
 module.exports = router;
